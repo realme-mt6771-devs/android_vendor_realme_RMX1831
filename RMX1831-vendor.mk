9c573da7688d38c3a7ebba3e0044d40fea631370
Sarthak Roy <sarthakroy2002@gmail.com>
2022-12-14 16:02:33 +0000

    RMX1831-vendor: drop emmc firmwares
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- RMX1831-vendor.mk
+++ RMX1831-vendor.mk
@@ -464,15 +464,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1831/proprietary/vendor/firmware/customfilter/catcher_filter_1_ulwctg_n__Default.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/customfilter/catcher_filter_1_ulwctg_n__Default.bin \
     vendor/realme/RMX1831/proprietary/vendor/firmware/customfilter/catcher_filter_1_ulwctg_n_meta.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/customfilter/catcher_filter_1_ulwctg_n_meta.bin \
     vendor/realme/RMX1831/proprietary/vendor/firmware/em_filter_1_ulwctg_n.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/em_filter_1_ulwctg_n.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/HYNIX-HCG8a4.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/HYNIX-HCG8a4.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/HYNIX-hB8aP>.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/HYNIX-hB8aP>.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/SAMSUNG-3V6CMB.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/SAMSUNG-3V6CMB.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/SAMSUNG-DH6DAB.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/SAMSUNG-DH6DAB.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/SAMSUNG-DH6DMB.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/SAMSUNG-DH6DMB.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/SAMSUNG-GD6BMB.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/SAMSUNG-GD6BMB.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/SAMSUNG-GP6BMB.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/SAMSUNG-GP6BMB.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/SAMSUNG-RH64AB.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/SAMSUNG-RH64AB.bin_crc.bin \
-    vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/SAMSUNG_RH64AB_3D-RH64AB.bin_crc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/SAMSUNG_RH64AB_3D-RH64AB.bin_crc.bin \
     vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/move_target_emmc_fw_to_opporeserve1.sh:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/move_target_emmc_fw_to_opporeserve1.sh \
     vendor/realme/RMX1831/proprietary/vendor/firmware/emmc/move_target_emmc_fw_to_reserve4.sh:$(TARGET_COPY_OUT_VENDOR)/firmware/emmc/move_target_emmc_fw_to_reserve4.sh \
     vendor/realme/RMX1831/proprietary/vendor/firmware/fastchg/17197/oppo_vooc_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fastchg/17197/oppo_vooc_fw.bin \
